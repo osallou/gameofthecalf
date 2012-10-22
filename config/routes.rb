9e4d3710562976c7cbed39870c2f925421feec00
Olivier Sallou <olivier.sallou@debian.org>
2012-10-22 16:07:53 +0200

    prevent user self registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 GameOfTheCalf::Application.routes.draw do
   resources :groups
 
-  devise_for :users
+  #devise_for :users
+  devise_for :users, :skip => [:registrations] 
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+
 
   resources :users
 
