6d42be04b078806776be9045a49479a19139f4dd
Olivier Sallou <olivier.sallou@gmail.com>
2012-10-23 11:33:15 -0400

    add fixtures and failing unit tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,4 +4,30 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "admin can create prof" do
+    assert false
+  end
+  
+  test "prof can create groups" do
+    assert false
+  end  
+  
+  test "prof can create students" do
+    assert false
+  end
+  
+  test "prof cannot create prof" do
+    assert false
+  end
+  
+  test "student cannot create student" do
+    assert false
+  end
+  
+  test "prof can change a student group" do
+    assert false
+  end
+  
+  
 end
