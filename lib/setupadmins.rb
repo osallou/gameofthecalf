e2826634c06046fe6c6e8f798e539a6bd325918d
Olivier Sallou <olivier.sallou@gmail.com>
2012-10-23 09:16:41 -0400

    Create admins only if not present in db

--- lib/setupadmins.rb
+++ lib/setupadmins.rb
@@ -1,14 +1,17 @@
 
 # Creates an account for each admin user
 Settings.admin.each do |admin|
-    user = User.new(:email => admin)
-    user.usertype = User::PROFESSOR
-    user.password = SecureRandom.hex(16)
-    user.password_confirmation =  user.password
-    user.confirm!
-    if user.save(:validate => false)
-      UserMailer.welcome_email(user).deliver
-    else
-      puts user.errors
+    # If user does not exists
+    if User.where(:email => admin).nil?
+      user = User.new(:email => admin)
+      user.usertype = User::PROFESSOR
+      user.password = SecureRandom.hex(16)
+      user.password_confirmation =  user.password
+      user.confirm!
+      if user.save(:validate => false)
+        UserMailer.welcome_email(user).deliver
+      else
+        puts user.errors
+      end
     end
 end
