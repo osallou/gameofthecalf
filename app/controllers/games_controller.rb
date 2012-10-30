4aeb7d2111685cc563414264b6fe108035d40f9f
Olivier Sallou <olivier.sallou@irisa.fr>
2012-10-30 09:09:45 +0100

    fix max levels use

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,10 +17,10 @@ class GamesController < ApplicationController
   # POST /games.json
   def create
     authorize! :create, Game
-    game = Game.new(:user_id => current_user.id)
-    game.save!
+    @game = Game.new(:user_id => current_user.id)
+    @game.save!
     @levels = []
-    level = Level.new(:game_id => game.id, :status => Level::STATUS_NEW, :level => 1)
+    level = Level.new(:game_id => @game.id, :status => Level::STATUS_NEW, :level => 1)
     level.save!
     @levels << level
     respond_to do |format|
@@ -32,8 +32,8 @@ class GamesController < ApplicationController
   # GET /games/1
   # GET /games/1.json
   def show
-    game = Game.find(params[:id])
-    @levels = Level.where(:game_id => game.id)
+    @game = Game.find(params[:id])
+    @levels = Level.where(:game_id => @game.id)
     respond_to do |format|
       format.html { render :levels } # levels.html.erb
       format.json { render json: @levels }
