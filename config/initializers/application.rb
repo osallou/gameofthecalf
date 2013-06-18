9e2824f2ab58fec2054ed1a605beb5e0c3b5f1a2
Olivier Sallou <olivier.sallou@irisa.fr>
2013-06-18 14:51:40 +0200

    init workdir to rails root if workdir is not set, needed for travis

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,5 +1,9 @@
 
-
-GameOfTheCalf::Application.config.pairtree = Pairtree.at(Settings.workdir, :prefix => 'bull:', :create => true)
+workdir = Settings.workdir
+if Settings.workdir.empty?
+    workdir = Rails.root.join('data')
+    Dir.mkdir(workdir)
+end
+GameOfTheCalf::Application.config.pairtree = Pairtree.at(workdir, :prefix => 'bull:', :create => true)
 
 puts "Initialize workdir at "+Settings.workdir
